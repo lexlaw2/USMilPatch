23d375d037c477801af6d6a6137d78635513aa3b
lexlaw <lexlaw2@ya.ru>
2019-02-08 17:05:47 +0700

    добавил всех юнитов 90х вудленд и убрал магазины

--- config.cpp
+++ config.cpp
@@ -2,7 +2,8 @@ class CfgPatches{
 
 	class clsBlockUSMilUnits
 	{
-		units[]={"usm_soldier","sm_soldier_90s_w_h_off"};
+		units[]={"usm_soldier","sm_soldier_90s_w_h_off","usm_soldier_90s_w_h_nco","usm_soldier_90s_w_h_ftl","usm_soldier_90s_w_h_gr","usm_soldier_90s_w_h_ar","usm_soldier_90s_w_h_at4","usm_soldier_90s_w_h_medic",
+		"usm_soldier_90s_w_h_rto","usm_soldier_90s_w_h_mg","usm_soldier_90s_w_h_amg","usm_soldier_90s_w_h_mgab","usm_soldier_90s_w_h_jav","usm_soldier_90s_w_h_ajav","usm_soldier_90s_w_h_aa","usm_soldier_90s_w_h_crew1"};
 		weapons[]={""};
 		requiredVersion=1;
 		requiredAddons[]={"us_military_units"};
@@ -31,10 +32,178 @@ class cfgVehicles {
 			respawnWeapons[] = {"Throw","Put"};
 			Items[] = {"usm_bdu_cap_wdl"};
 			RespawnItems[] = {"usm_bdu_cap_wdl"};
+			magazines[] = {};
+			respawnMagazines[] = {};
 			
+		};
+				
+		class usm_soldier_90s_w_h_nco:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};
+			magazines[] = {};
+			respawnMagazines[] = {};
+						
+		};
+		
+		class usm_soldier_90s_w_h_ftl:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};		
+magazines[] = {};
+			respawnMagazines[] = {};
+						
+		};
+		
+		class usm_soldier_90s_w_h_gr:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
 			
 		};
 		
+		class usm_soldier_90s_w_h_ar:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_at4:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_medic:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_rto:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_mg:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_amg:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_mgab:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_jav:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_ajav:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_aa:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
+		
+		class usm_soldier_90s_w_h_crew1:usm_soldier
+		{
+			scope = 0;
+			weapons[] = {"Throw","Put"};
+			respawnWeapons[] = {"Throw","Put"};
+			Items[] = {"usm_bdu_cap_wdl"};
+			RespawnItems[] = {"usm_bdu_cap_wdl"};				
+			magazines[] = {};
+			respawnMagazines[] = {};
+			
+		};
 	};
 
 
