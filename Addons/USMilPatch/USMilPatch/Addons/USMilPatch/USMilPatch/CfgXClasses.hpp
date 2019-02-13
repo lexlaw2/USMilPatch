c085233e87dac54d284faf7d623b25f7c6a8d12e
lexlaw <lexlaw2@ya.ru>
2019-02-13 13:43:33 +0700

    убрал мусор комментариев с вепонсов

---
+++ Addons/USMilPatch/USMilPatch/Addons/USMilPatch/USMilPatch/CfgXClasses.hpp
@@ -0,0 +1,62 @@
+// class CfgFactionClasses
+// {
+	// class usm_usarmy
+	// {
+		// displayName = "U.S. Army";
+		// priority = 8;
+		// primaryLanguage = "EN";
+		// side = "1";
+		// icon = "\US_Military_Units\icons\p_army.paa";
+		// flag = "\a3\Data_f\Flags\flag_us_co.paa";
+	// };
+	// class usm_usmarinecorps
+	// {
+		// displayName = "U.S. Marine Corps";
+		// priority = 8;
+		// primaryLanguage = "EN";
+		// side = "1";
+		// icon = "\US_Military_Units\icons\p_marinecorps.paa";
+		// flag = "\a3\Data_f\Flags\flag_us_co.paa";
+	// };
+	// class usm_usnavy
+	// {
+		// displayName = "U.S. Navy";
+		// priority = 8;
+		// primaryLanguage = "EN";
+		// side = "1";
+		// icon = "\US_Military_Units\icons\p_navy.paa";
+		// flag = "\a3\Data_f\Flags\flag_us_co.paa";
+	// };
+	// class usm_usairforce
+	// {
+		// displayName = "U.S. Air Force";
+		// priority = 8;
+		// primaryLanguage = "EN";
+		// side = "1";
+		// icon = "\US_Military_Units\icons\p_airforce.paa";
+		// flag = "\a3\Data_f\Flags\flag_us_co.paa";
+	// };
+// };
+// class CfgVehicleClasses
+// {
+	// class usm_90_w_men
+	// {
+		// displayName = "men (90s regular, woodland)";
+	// };
+	// class usm_90_d_men
+	// {
+		// displayName = "men (90s regular, desert)";
+	// };
+	// class usm_80_d_men
+	// {
+		// displayName = "men (Desert Storm)";
+	// };
+	// class usm_90_w_ranger
+	// {
+		// displayName = "men (90s Rangers, woodland)";
+	// };
+	// class usm_90_d_ranger
+	// {
+		// displayName = "men (90s Rangers, desert)";
+	// };
+// };
