eeb42b9c08496fe7988b1926510adf9898bcc724
lexlaw <lexlaw2@ya.ru>
2019-02-18 11:28:20 +0700

    добавил смену иконок для форм совместимых с броником

--- USMilPatch/usm_uniform.hpp
+++ USMilPatch/usm_uniform.hpp
@@ -10,6 +10,39 @@ class usm_bdu_w: Uniform_Base
 {
 	picture = "\USMilPatch\icons\modIcon.paa";
 };
+class usm_bdu_w_m: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+class usm_bdu_dcu: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+class usm_bdu_dcu_m: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+class usm_bdu_d: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+class usm_bdu_d_m: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+class usm_bdu_blk: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+class usm_bdu_odg: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+class usm_bdu_ubn: usm_bdu_w
+{
+	picture = "\USMilPatch\icons\modIcon.paa";
+};
+
 
 // class usm_bdu_cap_wdl: ItemCore
 	// {
