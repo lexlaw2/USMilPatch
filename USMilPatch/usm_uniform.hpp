4ce1f0d8022dcbf35a3ed4e6e6e8281b03e82782
lexlaw <lexlaw2@ya.ru>
2019-02-18 13:11:47 +0700

    убрал добавление картинки в измененые формы, ибо формы буду возращать в нормальное состояние, там и с иконкой разберусь, другим патчем

--- USMilPatch/usm_uniform.hpp
+++ USMilPatch/usm_uniform.hpp
@@ -10,18 +10,20 @@ class usm_bdu_w: Uniform_Base
 {
 	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
+
 class usm_bdu_w_m: usm_bdu_w
 {
 	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
-class usm_bdu_dcu: usm_bdu_w
-{
-	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
-};
-class usm_bdu_dcu_m: usm_bdu_w
-{
-	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
-};
+// игнорирую для патча
+// class usm_bdu_dcu: usm_bdu_w
+// {
+	// picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
+// };
+// class usm_bdu_dcu_m: usm_bdu_w
+// {
+	// picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
+// };
 class usm_bdu_d: usm_bdu_w
 {
 	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
@@ -30,18 +32,18 @@ class usm_bdu_d_m: usm_bdu_w
 {
 	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
-class usm_bdu_blk: usm_bdu_w
-{
-	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
-};
-class usm_bdu_odg: usm_bdu_w
-{
-	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
-};
-class usm_bdu_ubn: usm_bdu_w
-{
-	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
-};
+// class usm_bdu_blk: usm_bdu_w
+// {
+	// picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
+// };
+// class usm_bdu_odg: usm_bdu_w
+// {
+	// picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
+// };
+// class usm_bdu_ubn: usm_bdu_w
+// {
+	// picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
+// };
 
 
 // class usm_bdu_cap_wdl: ItemCore
---
+++ USMilUniformComebackPatch/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches{
+
+	class USMilUniformComebackPatch
+	{
+		requiredVersion=1;
+		requiredAddons[]={"us_military_uniforms","us_military_units"};
+	};
+	
+	
+};
