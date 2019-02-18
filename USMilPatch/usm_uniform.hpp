30d2d18d0bb55561970d45216afaab21ea178379
lexlaw <lexlaw2@ya.ru>
2019-02-18 11:41:59 +0700

    поменял префик и пбо, чтоб оба патча входили в одно семейство

--- USMilPatch/usm_uniform.hpp
+++ USMilPatch/usm_uniform.hpp
@@ -8,39 +8,39 @@ class usm_bdu_test: Uniform_Base
 };
 class usm_bdu_w: Uniform_Base
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_w_m: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_dcu: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_dcu_m: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_d: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_d_m: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_blk: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_odg: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 class usm_bdu_ubn: usm_bdu_w
 {
-	picture = "\USMilPatch\icons\modIcon.paa";
+	picture = "\USMilPatch\USMilPatch\icons\modIcon.paa";
 };
 
 
---
+++ USMilUniformPatch/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches{
+
+	class USMilPatch
+	{
+		requiredVersion=1;
+		requiredAddons[]={"us_military_uniforms"};
+	};
+	
+	
+};
