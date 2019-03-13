8f2e5a37df6637496fa23f3b7d2e1482bc92ed18
lexlaw <lexlaw2@ya.ru>
2019-03-13 16:43:18 +0700

    новые параметры веса и вместимости

--- usm_armor.hpp
+++ usm_armor.hpp
@@ -1,4 +1,5 @@
 // class HeadgearItem;
+
 class VestItem;
 
 	class usm_vest_LBE_rm: ItemCore
@@ -7,7 +8,8 @@ class VestItem;
 
 		class ItemInfo: VestItem
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 
 		};
 	};
@@ -17,7 +19,8 @@ class VestItem;
 		class ItemInfo: ItemInfo
 		{
 
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 
 		};
 	};
@@ -25,105 +28,120 @@ class VestItem;
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_LBE_mg: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_rm: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_rmp: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_gr: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_mg: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_LBE_rm_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_LBE_rmp_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_LBE_gr_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_LBE_mg_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_rm_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_rmp_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_gr_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_lbv_mg_m: usm_vest_LBE_rm
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply1000";
+			mass = 15;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt: ItemCore
 	{
 		class ItemInfo: VestItem
 		{
-			containerClass = "Supply100";
+			mass = 60;
+			containerClass = "Supply20";
 			class HitpointsProtectionInfo
 			{
 				class Neck
@@ -157,119 +175,136 @@ class VestItem;
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbe_rmp: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbe_gr: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbe_mg: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_rm: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_rmp: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_gr: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_mg: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbe_rm_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply140";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbe_rmp_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply10";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbe_gr_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbe_mg_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_rm_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_rmp_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_gr_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgt_lbv_mg_m: usm_vest_pasgt
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes: ItemCore
 	{
 		class ItemInfo: VestItem
 		{
-			containerClass = "Supply100";
+			mass = 60;
+			containerClass = "Supply20";
 			class HitpointsProtectionInfo
 			{
 				class Neck
@@ -282,19 +317,19 @@ class VestItem;
 				{
 					hitpointName = "HitChest";
 					armor = 16;
-					passThrough = 0.1;
+					passThrough = 0.7;
 				};
 				class Diaphragm
 				{
 					hitpointName = "HitDiaphragm";
 					armor = 16;
-					passThrough = 0.1;
+					passThrough = 0.7;
 				};
 				class Abdomen
 				{
 					hitpointName = "HitAbdomen";
 					armor = 16;
-					passThrough = 0.1;
+					passThrough = 0.7;
 				};
 			};
 		};
@@ -303,119 +338,136 @@ class VestItem;
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbe_rmp: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbe_gr: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbe_mg: usm_vest_pasgtdes
 	{
 	class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_rm: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_rmp: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_gr: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_mg: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbe_rm_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbe_rmp_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbe_gr_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbe_mg_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_rm_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_rmp_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_gr_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdes_lbv_mg_m: usm_vest_pasgtdes
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu: ItemCore
 	{
 		class ItemInfo: VestItem
 		{
-			containerClass = "Supply100";
+			mass = 60;
+			containerClass = "Supply20";
 			class HitpointsProtectionInfo
 			{
 				class Neck
@@ -449,126 +501,143 @@ class VestItem;
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbe_rmp: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbe_gr: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbe_mg: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_rm: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_rmp: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_gr: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_mg: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbe_rm_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbe_rmp_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbe_gr_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbe_mg_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_rm_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_rmp_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_gr_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_pasgtdcu_lbv_mg_m: usm_vest_pasgtdcu
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 101.3;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba: ItemCore
 	{
 		class ItemInfo: VestItem
 		{
-			containerClass = "Supply100";
+			mass = 100;
+			containerClass = "Supply20";
 			class HitpointsProtectionInfo
 			{
 				class Chest
 				{
 					hitpointName = "HitChest";
 					armor = 24;
-					passThrough = 0.4;
+					passThrough = 0.3;
 				};
 				class Diaphragm
 				{
@@ -589,62 +658,67 @@ class VestItem;
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba_lbe_rmp: usm_vest_rba
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba_lbe_gr: usm_vest_rba
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba_lbe_mg: usm_vest_rba
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba_lbv_rm: usm_vest_rba
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba_lbv_rmp: usm_vest_rba
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba_lbv_gr: usm_vest_rba
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_rba_lbv_mg: usm_vest_rba
 	{
 		class ItemInfo: ItemInfo
 		{
-			containerClass = "Supply100";
+			mass = 140;
+			containerClass = "Supply150";
 		};
 	};
 	class usm_vest_safety: ItemCore
 	{
-		class ItemInfo: VestItem
-		{
-			containerClass = "Supply100";
-		};
+	scope =0;
 	};
